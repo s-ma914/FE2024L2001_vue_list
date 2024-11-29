125ed83893df683b90bb50d59bcb91c530bd5cb0
s-ma914 <2301376@s.asojuku.ac.jp>
2024-11-29 09:50:39 +0900

    kadai

--- 5-1/script/script.js
+++ 5-1/script/script.js
@@ -2,7 +2,11 @@ new Vue({
     el: '#app',
     data() {
       return {
-        
+        items:[
+          { id: 1, label: "子"},
+          { id: 2, label: "丑"},
+          { id: 3, label: "寅"}
+        ]
       };
     }
   });
